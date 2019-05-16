2aa0be8b981f967bddd0e678db16730335b3a843
sgomera <37589649+sgomera@users.noreply.github.com>
2019-05-16 11:21:31 +0200

    Initial commit Fase3

---
+++ Coets_fase3/src/com/exCoets/Rocket.java
@@ -0,0 +1,67 @@
+package com.exCoets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rocket {
+    private String rocketId;
+    private List<Propellers> propellers;
+
+
+    //contructors
+    public Rocket(String rocketId, List propellers) throws Exception {
+        this.rocketId = rocketId;
+        this.propellers = propellers;
+
+        checkRocketId(rocketId);
+        checkPropellers(propellers);
+    }
+
+    public void checkRocketId(String rocketId) throws Exception {
+        if(this.rocketId.length()!= 8){
+            throw new Exception("l'Id del coet ha d'ésser de 8 caràcters");
+        }
+    }
+
+    public void checkPropellers(List propellers) throws Exception {
+        if(propellers.size() <= 0){
+            throw new Exception("El nombre de propulsors ha d'ésser més gran que 0");
+        }
+    }
+
+    public String getRocketId() {
+        return rocketId;
+    }
+
+    public void setRocketId(String rocketId) {
+        this.rocketId = rocketId;
+    }
+
+    public List<Propellers> getPropellers() {
+        return propellers;
+    }
+
+    public void setPropellers(List<Propellers> propellers) {
+        this.propellers = propellers;
+    }
+
+    public void  accRocket() throws Exception {
+
+            for (int i =0; i < propellers.size(); i++){
+                propellers.get(i).setCurrentPower(propellers.get(i).getCurrentPower() + 10);
+            }
+    }
+
+    public void brakeRocket() throws Exception {
+        for (int i =0; i < propellers.size(); i++){
+            propellers.get(i).setCurrentPower(propellers.get(i).getCurrentPower() - 10);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return rocketId  +  ": " +
+                propellers.toString().replace("[", "").replace("]", "");
+    }
+
+}
